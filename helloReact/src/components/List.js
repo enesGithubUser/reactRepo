697f7b88394470d21612b8aae9d82909837b97da
enesGithubUser <enes_gkaya@hotmail.com>
2019-08-17 18:46:15 +0300

    Update List.js
    List js guncellendi

--- helloReact/src/components/List.js
+++ helloReact/src/components/List.js
@@ -1,20 +1,37 @@
 import React, {Component} from 'react';
-import {View , Text} from 'react-native'
+import {View , Text} from 'react-native';
+import axios from 'axios';
 
 class List extends Component{
-componentWillMount(){
-    console.log('componentWillMount');
-}
-componentDidMount(){
-    console.log('componentDidMount');
-}
+    state = {veriler : []}
+    
+    componentWillMount(){
+        console.log('componentWillMount')
+        axios.get('http://reduxblog.herokuapp.com/api/posts')
+        .then(response =>{
+            console.log(response)
+            this.setState = ({veriler : response.data})
+        })       
+    }
+
+    componentDidMount(){
+        console.log('componentDidMount')
+    }
+    
+    renderData(){
+        return this.state.veriler.map(responseData =>
+            <Text>
+                {responseData.title}
+            </Text>
+        )
+    }
 
     render(){
-        console.log('render');
+        console.log('gelen data '  + this.state)
         return(
             <View style={{marginTop:5}}>
-                <Text> Icerik Listesi </Text>
-            </View> 
+                {this.renderData()}
+            </View>
         )
     }
 }
